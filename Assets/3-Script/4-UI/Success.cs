4af6e9bcfc1e261c7a1d61e2247a8699261defcc
Monday <110732428+takemiethere@users.noreply.github.com>
2023-03-16 13:48:43 +0700

    add UI
    +ทำได้ละเหลือเปิดปิดตอนเกมจบ แล้วก็แก้การคำนวณนิดหน่อย

--- Assets/3-Script/4-UI/Success.cs
+++ Assets/3-Script/4-UI/Success.cs
@@ -14,9 +14,66 @@ public class Success : MonoBehaviour
     public Image progressBar;
     public TextMeshProUGUI percentageText;
 
+    int totalTasks;
+    int completedTasks;
     private int star = 5;
     private int fromfive = 0;
     private int getfive = 0;
+
+    private bool timerUp = false;
+    private TimerAndScore timerScript;
+
+    void Start()
+    {
+        trashScript = FindObjectOfType<DestroyChild>();
+        dustScript = FindObjectOfType<Item>();
+        bugScript = FindObjectOfType<BugDestroyUI>();
+        ratScript = FindObjectOfType<RatDestroyUI>();
+        timerScript = FindObjectOfType<TimerAndScore>();
+    }
+
+    private void Update()
+    {
+        if (timerScript.timeUp && !timerUp)
+        {
+            timerUp = true;
+            totalTasks = trashScript.totalBin + dustScript.totalDust + bugScript.totalBug + ratScript.totalRat;
+
+            percentageText.text = "Star: " + fromfive + "%";
+        }
+
+        if (percentageText.enabled)
+        {
+            completedTasks = trashScript.doneBin + dustScript.doneDust + bugScript.doneBug + ratScript.doneRat;
+            fromfive = completedTasks * 100 / totalTasks;
+
+            progressBar.fillAmount = (float)fromfive / 100;
+        }
+    }
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Success : MonoBehaviour
+{
+    private DestroyChild trashScript;
+    private Item dustScript;
+    private BugDestroyUI bugScript;
+    private RatDestroyUI ratScript;
+
+    public Image progressBar;
+    public TextMeshProUGUI percentageText;
+
+    private int star = 5;
+    public int fromfive = 0;
+    private int getfive = 0;
+
+    public int totalTasks =0;
+    public int completedTasks =0;
 
     void Start()
     {
@@ -28,11 +85,11 @@ public class Success : MonoBehaviour
 
     private void Update()
     {
-        int totalTasks = trashScript.totalBin + dustScript.totalDust + bugScript.totalBug + ratScript.totalRat;
-        int completedTasks = trashScript.doneBin + dustScript.doneDust + bugScript.doneBug + ratScript.doneRat;
+        totalTasks = trashScript.totalBin + dustScript.totalDust + bugScript.totalBug + ratScript.totalRat;
+        completedTasks = trashScript.doneBin + dustScript.doneDust + bugScript.doneBug + ratScript.doneRat;
         fromfive = completedTasks * 100 / totalTasks;
 
         progressBar.fillAmount = (float)fromfive / 100;
         percentageText.text = "Star: " + fromfive + "%";
     }
-}*/
+}
--- Assets/3-Script/4-UI/TimerAndScore.cs
+++ Assets/3-Script/4-UI/TimerAndScore.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
@@ -6,7 +6,7 @@ public class TimerAndScore : MonoBehaviour
 {
     public float timeLeft = 3.0f;
     public TextMeshProUGUI timerText;
-    private bool timeUp = false;
+    public bool timeUp = false;
     public Image timerBarImage;
 
     //public Success starManagerScript;
@@ -25,6 +25,77 @@ public class TimerAndScore : MonoBehaviour
             TimeUp();
         }
 
+    }
+
+
+    void TimeUp()
+    {
+        Time.timeScale = 0;
+        timerText.text = "Time's up!";
+        // disable player's input script
+        this.enabled = false;
+    }
+
+}*/
+
+
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerAndScore : MonoBehaviour
+{
+    public float timeLeft = 3.0f;
+    public TextMeshProUGUI timerText;
+    /*public int score = 0;
+    public TextMeshProUGUI scoreText;*/
+    private bool timeUp = false;
+    public Image timerBarImage;
+
+    public Success starManagerScript;
+
+    void Start()
+    {
+        starManagerScript = FindObjectOfType<Success>();
+    }
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(timeLeft / 60.0f);
+        int seconds = Mathf.FloorToInt(timeLeft % 60.0f);
+        timerText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+        timerBarImage.fillAmount = timeLeft / 180.0f; // Set the fill amount based on the remaining time
+
+        if (timeLeft < 0)
+        {
+            timeUp = true;
+            TimeUp();
+            starManagerScript.enabled = true;
+        }
+
+
+        if (!timeUp)
+        {
+            if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.CompareTag("Enemy"))
+                    {
+                        Destroy(hit.transform.gameObject);
+                        /*                        score++;
+                                                scoreText.text = score + " frogs was ate.";
+
+                                                Debug.Log("You ate: " + score + "Frogs");*/
+                    }
+                }
+            }
+        }
     }
 
 
